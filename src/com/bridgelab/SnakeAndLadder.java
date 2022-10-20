fec6f4adce663d0de3caca68a7ebcd1fc54e26cf
Saiprasad8341 <saiprasad.8341@gmail.com>
2022-10-20 15:39:09 +0530

    Welcome to Snake and Ladder game

---
+++ src/com/bridgelab/SnakeAndLadder.java
@@ -0,0 +1,7 @@
+package com.bridgelab;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Snake and Ladder Program by using Java");
+    }
+}
