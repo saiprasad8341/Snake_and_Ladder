ff9732c86df4a55a46ab009eea9c39fe148eaa81
Saiprasad8341 <saiprasad.8341@gmail.com>
2022-10-20 20:18:35 +0530

    UC1 - Snake and Ladder game played with single player at start position 0

--- src/com/bridgelab/SnakeAndLadder.java
+++ src/com/bridgelab/SnakeAndLadder.java
@@ -2,6 +2,13 @@ package com.bridgelab;
 
 public class SnakeAndLadder {
     public static void main(String[] args) {
+        //UC1 - Snake and Ladder game played with single player at start Position 0
         System.out.println("Welcome to the Snake and Ladder Program by using Java");
+
+        int position = 0;
+
+
+        System.out.println("Payer starts at "+ position + " position");
+
     }
 }
